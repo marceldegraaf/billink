65255b047c6a5a6c149dc53eacda63bb99c0686a
Marcel de Graaf <mail@marceldegraaf.net>
2014-07-16 23:21:58 +0200

    Add check result reason to check

--- lib/billink/check.rb
+++ lib/billink/check.rb
@@ -2,21 +2,24 @@ module Billink
   class Check
     include Billink::Helpers::AttributeAssignment
 
-    attr_accessor :client, :uuid
+    attr_accessor :client, :uuid, :reason
 
     def perform
       Billink.log("Performing check (company name: #{client.company_name}, kvk number: #{client.kvk_number})")
 
       if response.success?
         @result = response.description == "Advies=1"
+        @reason = ""
         @uuid = response.uuid
       else
         Billink.log("Billink API returned an error: #{response.description}")
+        @reason = response.description
         @result = false
       end
 
     rescue => e
       Billink.log("Billink API request failed: #{e.message}")
+      @reason = e.message
       @result = false
 
     end
